fbdbb5e6b462de62648d1379647b6b97fcc632ff
Md Abir Hossain <hossain.mdabir@gmail.com>
2023-04-06 06:21:42 +0600

    Project created and asset files are added.

---
+++ Fructus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Md Abir Hossain on 6/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Md Abir Hossain on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
